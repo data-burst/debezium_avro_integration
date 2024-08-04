16459bdc6bbe265bb2fbc241aa0c14fc20a42efb
Niyusha Baghayi <niyusha.baghayi@gmail.com>
2024-08-04 18:52:42 +0330

    :bug: fix: change scripts in a way that change the unzip flag to have files flat

--- examples/mysql-avro-mongodb/scripts/jarfile_downloader.sh
+++ examples/mysql-avro-mongodb/scripts/jarfile_downloader.sh
@@ -6,19 +6,20 @@ echo "Creating directory: $JAR_FILES_DIR"
 mkdir -p $JAR_FILES_DIR
 
 MONGO_KAFKA_CONNECT_VERSION=1.13.0
-ARTIFACT="https://github.com/mongodb/mongo-kafka/releases/download/r${MONGO_KAFKA_CONNECT_VERSION}/mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}.zip"
+MONGO_KAFKA_CONNECT_DIR_NAME=mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}
+ARTIFACT="https://github.com/mongodb/mongo-kafka/releases/download/r${MONGO_KAFKA_CONNECT_VERSION}/${MONGO_KAFKA_CONNECT_DIR_NAME}.zip"
 
 
 
 echo $ARTIFACT
 TMP_DIR=/tmp/mongodb
-JARFILE_NAME=mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}
+# ZIP_NAME=mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}.zip
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 wget --continue --quiet --show-progress --progress=bar:force --directory-prefix $TMP_DIR $ARTIFACT
 
-unzip ${JARFILE_NAME}.zip
-cp $JARFILE_NAME/lib/mongo-kafka-connect-${MONGO_KAFKA_CONNECT_VERSION}-confluent.jar ${JAR_FILES_DIR}
+unzip -j ${MONGO_KAFKA_CONNECT_DIR_NAME}.zip
+cp mongo-kafka-connect-${MONGO_KAFKA_CONNECT_VERSION}-confluent.jar ${JAR_FILES_DIR}
 rm -rf $TMP_DIR
 
 echo "Done"
