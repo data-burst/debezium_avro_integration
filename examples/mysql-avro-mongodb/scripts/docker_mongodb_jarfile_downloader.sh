c8a9cfe47895583c217c6409e9499c69b093ab20
Niyusha Baghayi <niyusha.baghayi@gmail.com>
2024-08-04 19:05:14 +0330

    :bug: fix: add MONGO_KAFKA_CONNECT_VERSION to docker script

--- examples/mysql-avro-mongodb/scripts/docker_mongodb_jarfile_downloader.sh
+++ examples/mysql-avro-mongodb/scripts/docker_mongodb_jarfile_downloader.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 
-JAR_FILES_DIR=/kafka/connect
+JAR_FILES_DIR=/tmp/connect
 echo "Creating directory: $JAR_FILES_DIR"
 mkdir -p $JAR_FILES_DIR
 
+MONGO_KAFKA_CONNECT_VERSION=1.13.0
 MONGO_KAFKA_CONNECT_DIR_NAME=mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}
 ARTIFACT="https://github.com/mongodb/mongo-kafka/releases/download/r${MONGO_KAFKA_CONNECT_VERSION}/${MONGO_KAFKA_CONNECT_DIR_NAME}.zip"
 
 echo $ARTIFACT
 TMP_DIR=/tmp/mongodb
-# ZIP_NAME=mongodb-kafka-connect-mongodb-${MONGO_KAFKA_CONNECT_VERSION}.zip
 mkdir -p $TMP_DIR
 cd $TMP_DIR
 curl --show-error --location --progress-bar --remote-name --output-dir $TMP_DIR $ARTIFACT
