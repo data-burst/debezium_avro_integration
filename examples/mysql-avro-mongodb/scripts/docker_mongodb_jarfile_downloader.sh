4f2e73d411d5b9822f915b989e778100d1ac70ed
Mostafa Ghadimi <mostafa.ghadimi@yahoo.com>
2024-08-04 16:08:33 +0330

    :wrench: fix: resolve the value of MONGO_KAFKA_CONNECT_VERSION

--- examples/mysql-avro-mongodb/scripts/docker_mongodb_jarfile_downloader.sh
+++ examples/mysql-avro-mongodb/scripts/docker_mongodb_jarfile_downloader.sh
@@ -4,6 +4,7 @@ JAR_FILES_DIR="/kafka/connect"
 echo "Creating directory: $JAR_FILES_DIR"
 mkdir -p $JAR_FILES_DIR
 
+MONGO_KAFKA_CONNECT_VERSION=1.13.0
 ARTIFACTS_URL=(
   # Mongo Connect
   "https://repo1.maven.org/maven2/org/mongodb/kafka/mongo-kafka-connect/${MONGO_KAFKA_CONNECT_VERSION}/mongo-kafka-connect-${MONGO_KAFKA_CONNECT_VERSION}.jar"
